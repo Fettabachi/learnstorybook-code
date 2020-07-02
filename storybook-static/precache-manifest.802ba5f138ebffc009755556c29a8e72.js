ffccb2dae6a71d2a00b6d835013454c095d1889b
Tim Fetter <tim.fetter@magicjack.com>
2020-07-02 12:55:43 -0400

    Build storybook

---
+++ storybook-static/precache-manifest.802ba5f138ebffc009755556c29a8e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f8fa340184f3d8f25d32edc67ed8d40",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.3fcde0df0c17e29f29f6.bundle.js"
+  },
+  {
+    "url": "runtime~main.3fcde0df0c17e29f29f6.bundle.js"
+  },
+  {
+    "revision": "87b1409cbe9964d66b76",
+    "url": "static/css/main.35000dfd.chunk.css"
+  },
+  {
+    "url": "vendors~main.3fcde0df0c17e29f29f6.bundle.js"
+  },
+  {
+    "url": "vendors~main.3fcde0df0c17e29f29f6.bundle.js.LICENSE.txt"
+  }
+]);
